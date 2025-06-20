d185ff0f44c63c4bd59ce9e30f54b107882911ad
Tal Hayut <talhayut.dev@gmail.com>
2025-06-20 16:39:17 +0300

    chore: slightly more compelling loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { useProgress } from '@react-three/drei'
 import gsap from 'gsap'
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin'
 import { SplitText } from 'gsap/SplitText'
 import { useLayoutEffect } from 'react'
 
 gsap.registerPlugin(SplitText)
+gsap.registerPlugin(ScrambleTextPlugin)
 
 import s from './loader.module.scss'
 
@@ -21,10 +23,6 @@ export default function Loader() {
     })
 
     const tl = gsap.timeline()
-    tl.to(nums.chars, {
-      '-webkit-text-fill-color': 'white',
-      stagger: 0.08
-    })
 
     if (progress === 100) {
       tl.to(`.${s.title}`, { visibility: 'visible', delay: 1 }, '-=1')
@@ -35,6 +33,7 @@ export default function Loader() {
           ease: 'expo.inOut',
           yPercent: -300,
           stagger: 0.08,
+          '-webkit-text-fill-color': 'white',
           duration: 2
         },
         '<'
@@ -54,11 +53,11 @@ export default function Loader() {
         '-=2.3'
       )
 
-      tl.to(nums.chars, {
+      tl.to([nums.chars, chars], {
         ease: 'expo.inOut',
         y: '-100vh',
         stagger: 0.08,
-        duration: 1.5
+        duration: 2
       })
 
       tl.to(
@@ -70,28 +69,17 @@ export default function Loader() {
       )
 
       tl.to(
-        chars,
-        {
-          ease: 'expo.inOut',
-          y: '-100vh',
-          stagger: 0.08,
-          duration: 2
-        },
-        '<'
-      )
-      tl.to(
         `.${s.loader}`,
         {
-          opacity: 0,
-          duration: 0
+          y: '-100vh'
         },
         '-=1'
       )
 
-      tl.to(`.${s.loader}`, {
-        display: 'none',
-        duration: 0
-      })
+      // tl.to(`.${s.loader}`, {
+      //   display: 'none',
+      //   duration: 0
+      // })
     }
     tl.play()
   }, [progress])
@@ -100,8 +88,10 @@ export default function Loader() {
 
   return (
     <div className={s.loader}>
-      <div className={s.title}>MATCAPS</div>
-      <div className={s.loading}>{prog}</div>
+      <div className={s.wrapper}>
+        <div className={s.title}>MATCAPS</div>
+        <div className={s.loading}>{prog}</div>
+      </div>
       {/* <img src="/textures/matcaps/matcap_16.png" className={s.img} /> */}
     </div>
   )
